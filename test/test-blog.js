115c4fa3cd78935a8bdc93c4e2a2dbacee45561f
chris.bridges <christopher_bridges@icloud.com>
2018-03-22 08:53:32 -0700

    starting seedData func

---
+++ test/test-blog.js
@@ -0,0 +1,26 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// this makes the expect syntax available throughout
+// this module
+const expect = chai.expect;
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config'); // declare TestDatabaseURL in config.js
+
+chai.use(chaiHttp);
+
+function seedPostData () {
+  console.info('seeding post data');
+  const seedData = [];
+
+  for (let i = 0; i < 10; i++) {
+    seedData.push(generatePostData());
+  }
+  
+  return BlogPost.insertMany(seedData);
+}
